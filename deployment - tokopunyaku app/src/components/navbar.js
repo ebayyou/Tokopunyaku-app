5c56f47e4aabf517c307b381cb7978e45787ef35
ebayyou <ebayyouanggoro.e@gmail.com>
2022-09-20 16:29:10 -0700

    Tokopunyaku - app : update

--- deployment - tokopunyaku app/src/components/navbar.js
+++ deployment - tokopunyaku app/src/components/navbar.js
@@ -11,19 +11,31 @@ class Navbar extends HTMLElement {
   render() {
     this._shadowRoot.innerHTML = `
         <style>
-          nav {
+          header {
             background-color: thistle;
             padding: 0.4em 0.6em;
           }
 
           nav h1 {
-            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            font-family: 'Satoshi', sans-serif;
+            font-weight: 800;
           }
         </style>
 
-        <nav>
-          <h1>Navbar</h1>
-        </nav>
+        <header>
+          <div class="header__brand">
+            <h1 class="header__name">Tokopunyaku</h1>
+            <img src='../assets/Logo - favicon/tokopunyaku-app-32.png' class="header__logo">
+          </div>
+          
+          <nav class="nav">
+            <ul class="nav__group">
+              <li class="nav__list"><a href="" class="nav__item"></a></li>
+              <li class="nav__list"><a href="" class="nav__item"></a></li>
+              <li class="nav__list"><a href="" class="nav__item"></a></li>
+            </ul>
+          </nav>
+        </header>
     `;
   }
 }
